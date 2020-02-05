887ef0b8ab8f5fd588d2af43447378f71a921255
ariessandi <ariessandi7@gmail.com>
2020-02-05 15:42:22 +0700

    exercise 8-9
    done

--- testodos.js
+++ testodos.js
@@ -43,6 +43,79 @@ console.log(depan);
 
 
 
+
+var todos = [
+  ["makan",3],
+  ["belajar",2],
+  ["game",1]
+];
+
+var log=["makan","belajar","game"];
+
+for(var i=0; i < log.length; i++){
+  for(var j=0; j < todos.length; j++){
+    if(log[i] === todos[j][0]){
+      todos[j][1]--;
+    }
+  }
+}
+
+console.log(todos);
+
+
+
+
+
+
+
+
+//var nomor = [1,2,3,4,5,6,7,8,9,10,11,12];
+/*
+var x=0;
+var y=[];
+for(var i=1;i< nomor.length; i++){
+  if(i%3 == 0){
+    
+    y[x]=nomor;
+    x= x+1;
+  }
+}
+
+console.log(y);
+*/
+function numbers(rows,colums){
+  var nomor = [1,2,3,4,5,6,7,8,9,10,11,12];
+  var x=0;
+  var arr=[];
+  var y=[];
+  var z=0;
+for(var i=0;i< rows; i++){
+
+ x=(i+1)*colums - colums;
+
+ z= colums + x;
+  for(var j=x; j< z;j++){
+      
+        arr.push(nomor[j]);
+     
+     x=x+1;
+  }
+  y[i]=arr;
+  //return arr;
+  
+  arr=[];
+//return y[0]=arr;
+
+}
+console.log(y);
+
+}
+
+console.log(numbers(4,3));
+
+
+
+
 //for(var i = 0; i < student.length ; i++){
   //for(var j=0; j < kamus.length; j++){
 
