8b95a42749e046255770cd66cb67326a4a2b110c
ariessandi <ariessandi7@gmail.com>
2020-02-12 17:19:05 +0700

    Update latian_flag.js

--- latian_flag.js
+++ latian_flag.js
@@ -54,3 +54,101 @@ for(var i = 0; i < arr.length; i++ ){
   }
 }
 console.log(ress);
+
+
+
+
+
+
+OBJEK
+
+
+
+
+function highestScore (students) {
+  // Code disini
+  var obj ={};
+  var stud = [];
+
+    for(var l = 0; l < students.length; l++){  
+       //if(students[l].score > 75 ){ 
+            stud= {nama : students[l].name, score :students[l].score};
+          //} 
+    }
+    obj[l]=stud;
+    stud=[];
+
+
+    return obj;
+
+}
+
+
+
+
+// TEST CASE
+console.log(highestScore([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]));
+
+// {
+//   foxes: { name: 'Dimitri', score: 90 },
+//   wolves: { name: 'Alexei', score: 85 }
+// }
+
+
+console.log(highestScore([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]));
+
+// {
+//   foxes: { name: 'Alexander', score: 100 },
+//   wolves: { name: 'Alisa', score: 76 },
+//   tigers: { name: 'Viktor', score: 80 }
+// }
+
+
+console.log(highestScore([])); //{}
