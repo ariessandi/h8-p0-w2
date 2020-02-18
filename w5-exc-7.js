6724c1572638a26b0b8945d5783123e500760532
ariessandi <ariessandi7@gmail.com>
2020-02-18 16:40:26 +0700

    Update w5-exc-7.js
    revisi

--- w5-exc-7.js
+++ w5-exc-7.js
@@ -1,24 +1,21 @@
-function hapusSimbol(str) {
-  // you can only write your code here!
-  var kamus ="1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var arr = [];
-  for(var j = 0; j < str.length; j++){
-  for(var i = 0; i < kamus.length; i++){
-    
-      if(str[j]==kamus[i]){
-        arr.push(str[j]);
-      }
-    }
-  
-  }
-  //console.log(arr);
-  var hasil ="";
- for(var a = 0; a < arr.length; a++){
-    hasil += arr[a];
- }
-
-   return hasil;
+function hapusSimbol(str){
+var arr=[];
+for(var i = 0; i < str.length; i++){
+  arr.push(str[i]);
 }
+  var hasil = "";
+  var patt = /^[a-zA-Z0-9]+$/;
+  for(var a = 0; a < arr.length; a++){
+    if(arr[a].match(patt)){
+      //console.log(arr[a]);
+      hasil += arr[a];
+    }
+   
+  }
+//console.log(hasil);
+return hasil;
+}
+
 
 // TEST CASES
 console.log(hapusSimbol('test%$4aa')); // test4aa
